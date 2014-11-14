90fbc194c2f2913c352859832fc0a4cbd1ac8271
dwightgnjohnson <dwightgnjohnson@gmail.com>
2014-11-14 17:07:11 -0600

    first and last commit, I hope

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+["Alpine Meadows Ski Resort", "Squaw Valley Ski Resort", "Sugar Bowl Ski Resort Norden CA", "Boreal Mountain Resort Truckee CA", "Homewood Mountain, Homewood CA" ].each do |name|
+
+  Point.create name: name
+end
